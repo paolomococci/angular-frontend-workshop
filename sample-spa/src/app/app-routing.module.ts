cffce4de8fd4a23c4ec24e8d789f38bd300f0a36
paolomococci <paolomococci@gmail.com>
2021-10-14 03:38:17 +0200

    update of some dependencies

--- sample-spa/src/app/app-routing.module.ts
+++ sample-spa/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+const routes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: 'second', component: SecondComponent },
+  { path: '', redirectTo: '/first', pathMatch: 'full' },
+  { path: '**', component: FirstComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
