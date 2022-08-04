d9e1cf4a3201b2a99b800cae711eb842095b4b2a
paolomococci <paolomococci@gmail.com>
2022-08-04 16:41:22 +0200

    modified:   open-see-get/webpack.prod.config.js

--- open-see-get/webpack.prod.config.js
+++ open-see-get/webpack.prod.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const baseConfig = require('./webpack.config')
 
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   ...baseConfig,
   output: {
