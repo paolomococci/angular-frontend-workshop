4a75165d0f7a0215df4480818c70ade856fadf14
paolomococci <paolomococci@gmail.com>
2022-08-04 11:16:39 +0200

    modified:   open-see-get/src/app/components/editor/editor.component.ts

--- open-see-get/src/app/components/editor/editor.component.ts
+++ open-see-get/src/app/components/editor/editor.component.ts
@@ -3,6 +3,10 @@ import {
   OnInit
 } from '@angular/core'
 
+import {
+  EditorService
+} from './../../services/editor.service'
+
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
