beddbebf73d2744103262f65d34ccd2fd70e6517
paolomococci <paolomococci@gmail.com>
2022-08-04 09:15:33 +0200

    modified:   open-see-get/webpack.config.js

--- open-see-get/webpack.config.js
+++ open-see-get/webpack.config.js
@@ -1,6 +1,18 @@
-const path = require('path')
+const path = require('path');
 const src = path.join(
   process.cwd(),
   'src',
   'electron'
-)
+);
+
+module.exports = {
+  entry: path.join(src, 'main.ts'),
+  output: {
+    path: path.join(
+      process.cwd(),
+      'dist',
+      'open-see-get'
+    ),
+    filename: 'shell.js'
+  }
+};
