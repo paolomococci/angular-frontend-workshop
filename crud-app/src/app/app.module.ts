ebfaeddf9513149fe5146464d2ac4c6631006be7
paolomococci <paolomococci@gmail.com>
2022-08-16 07:11:27 +0200

    modified:   crud-app/src/app/app.module.ts

--- crud-app/src/app/app.module.ts
+++ crud-app/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
+import { FruitsComponent } from './components/fruits/fruits.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FruitsComponent
   ],
   imports: [
     BrowserModule,
