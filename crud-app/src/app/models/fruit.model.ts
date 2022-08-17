23455a2c7f7b35c199f0e4d5db3163b2cd381fc3
paolomococci <paolomococci@gmail.com>
2022-08-17 16:59:02 +0200

    modified:   crud-app/src/app/models/fruit.model.ts

--- crud-app/src/app/models/fruit.model.ts
+++ crud-app/src/app/models/fruit.model.ts
@@ -1,4 +1,25 @@
 export interface Fruit {
   id: number
   name: string
+  calories: number | null // kcal
+  carbohydrate: number | null // g
+  fiber: number | null // g
+  sugar: number | null // g
+  fat: number | null // g
+  protein: number | null // g
+  vitaminA: number | null // % RDA
+  vitaminB1: number | null // % RDA
+  vitaminB2: number | null // % RDA
+  vitaminB3: number | null // % RDA
+  vitaminB5: number | null // % RDA
+  vitaminB6: number | null // % RDA
+  vitaminB7: number | null // % RDA
+  vitaminB9: number | null // % RDA
+  vitaminB12: number | null // % RDA
+  vitaminC: number | null // % RDA
+  vitaminD: number | null // % RDA
+  vitaminE: number | null // % RDA
+  calcium: number | null // % RDA
+  potassium: number | null // % RDA
+  manganese: number | null // % RDA
 }
