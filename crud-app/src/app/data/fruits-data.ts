f53a4bd485883ccff6422aa3f4d153d7f8d3735b
paolomococci <paolomococci@gmail.com>
2022-08-17 17:22:13 +0200

    modified:   crud-app/src/app/data/fruits-data.ts

--- crud-app/src/app/data/fruits-data.ts
+++ crud-app/src/app/data/fruits-data.ts
@@ -2,6 +2,6 @@ import { Fruit } from "../models/fruit.model"
 
 export class FruitsData {
   static fruits: Fruit[] = [
-    {id:1,name:'apple',calories:null,carbohydrate:null,fiber:null,sugar:null,fat:null,protein:null,vitaminA:null,vitaminB1:null,vitaminB2:null,vitaminB3:null,vitaminB5:null,vitaminB6:null,vitaminB7:null,vitaminB9:null,vitaminB12:null,vitaminC:null,vitaminD:null,vitaminE:null,vitaminK:null,calcium:null,copper:null,iron:null,magnesium:null,manganese:null,phosphorus:null,potassium:null},
+    {id:1,name:'apple',calories:null,carbohydrate:null,fiber:null,sugar:null,fat:null,protein:null,vitaminA:null,vitaminB1:null,vitaminB2:null,vitaminB3:null,vitaminB5:null,vitaminB6:null,vitaminB7:null,vitaminB9:null,vitaminB12:null,vitaminC:null,vitaminD:null,vitaminE:null,vitaminK:null,calcium:null,copper:null,iron:null,magnesium:null,manganese:null,phosphorus:null,potassium:null,selenium:null},
   ]
 }
