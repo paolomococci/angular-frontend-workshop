da68a536f7ab89b0e5d1b8b6ca86a04d8263d875
paolomococci <paolomococci@gmail.com>
2022-08-19 11:09:45 +0200

    modified:   crud-app/src/app/components/fruit-details/fruit-details.component.spec.ts

--- crud-app/src/app/components/fruit-details/fruit-details.component.spec.ts
+++ crud-app/src/app/components/fruit-details/fruit-details.component.spec.ts
@@ -1,10 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing'
 
-import { FruitDetailsComponent } from './fruit-details.component';
+import { FruitDetailsComponent } from './fruit-details.component'
 
 describe('FruitDetailsComponent', () => {
-  let component: FruitDetailsComponent;
-  let fixture: ComponentFixture<FruitDetailsComponent>;
+  let component: FruitDetailsComponent
+  let fixture: ComponentFixture<FruitDetailsComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,12 +15,12 @@ describe('FruitDetailsComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FruitDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(FruitDetailsComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
