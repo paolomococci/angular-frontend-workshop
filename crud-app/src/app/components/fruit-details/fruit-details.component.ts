b9af59b4da47968eec74714aebbc3917c4ffaf12
paolomococci <paolomococci@gmail.com>
2022-08-19 11:07:14 +0200

    new file:   crud-app/src/app/components/fruit-details/fruit-details.component.ts

---
+++ crud-app/src/app/components/fruit-details/fruit-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fruit-details',
+  templateUrl: './fruit-details.component.html',
+  styleUrls: ['./fruit-details.component.sass']
+})
+export class FruitDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
