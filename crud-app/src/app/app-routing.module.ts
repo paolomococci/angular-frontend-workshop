10657daefb41f6778aab39a1009a7ed6a9aa07a2
paolomococci <paolomococci@gmail.com>
2022-08-21 10:16:53 +0200

    modified:   crud-app/src/app/app-routing.module.ts

--- crud-app/src/app/app-routing.module.ts
+++ crud-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { FruitsComponent } from './components/fruits/fruits.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'fruits',
     component: FruitsComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   },
   {
     path: '**',
