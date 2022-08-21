70397350fb0325f8b87d9c68205263ac68e7a5fe
paolomococci <paolomococci@gmail.com>
2022-08-21 10:17:23 +0200

    modified:   crud-app/src/app/app-routing.module.ts

--- crud-app/src/app/app-routing.module.ts
+++ crud-app/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component'
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/fruits',
+    redirectTo: '/dashboard',
     pathMatch: 'full'
   },
   {
