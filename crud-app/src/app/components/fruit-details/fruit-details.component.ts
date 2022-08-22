ba250547c8b9debc6a284eac5e2102f5151b9513
paolomococci <paolomococci@gmail.com>
2022-08-22 06:06:01 +0200

    modified:   crud-app/src/app/components/fruit-details/fruit-details.component.ts

--- crud-app/src/app/components/fruit-details/fruit-details.component.ts
+++ crud-app/src/app/components/fruit-details/fruit-details.component.ts
@@ -3,8 +3,11 @@ import {
   OnInit,
   Input
 } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { Location } from '@angular/common'
 
 import { Fruit } from 'src/app/models/fruit.model'
+import { FruitService } from 'src/app/services/fruit.service'
 
 @Component({
   selector: 'app-fruit-details',
