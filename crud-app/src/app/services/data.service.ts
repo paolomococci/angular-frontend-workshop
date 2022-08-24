0579d4b8d3fe435dfe256347f6ffeb7b3df0a961
paolomococci <paolomococci@gmail.com>
2022-08-24 09:30:09 +0200

    modified:   crud-app/src/app/services/data.service.ts

--- crud-app/src/app/services/data.service.ts
+++ crud-app/src/app/services/data.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core'
-import { InMemoryDbService } from 'angular-in-memory-web-api'
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api'
+import { Observable } from 'rxjs'
 
 import { FruitsData } from '../data/fruits-data'
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService implements InMemoryDbService {
 
   constructor() { }
+  createDb(reqInfo?: RequestInfo | undefined): {} | Observable<{}> | Promise<{}> {
+    throw new Error('Method not implemented.')
+  }
 }
