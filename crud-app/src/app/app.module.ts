79fcfd183d99375be3444d1aac5037a9e9eef44a
paolomococci <paolomococci@gmail.com>
2022-08-24 09:21:00 +0200

    modified:   crud-app/src/app/app.module.ts

--- crud-app/src/app/app.module.ts
+++ crud-app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
@@ -23,7 +25,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component'
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
