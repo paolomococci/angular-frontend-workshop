86546080af96ca74cd743fa052f165a25fd04317
paolomococci <paolomococci@gmail.com>
2022-08-24 10:52:59 +0200

    modified:   crud-app/src/app/services/fruit.service.ts

--- crud-app/src/app/services/fruit.service.ts
+++ crud-app/src/app/services/fruit.service.ts
@@ -63,6 +63,8 @@ export class FruitService {
     return fruits
   }
 
+  getFruitById(): void {}
+
   getFruitsThanksToHttpClient(): Observable<Fruit[]> {
     return this.httpClient
       .get<Fruit[]>(
