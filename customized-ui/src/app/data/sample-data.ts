53c0f69c5e1baf3a00ab31dddcbad1c4362e5c5a
paolomococci <paolomococci@gmail.com>
2022-09-30 20:57:19 +0200

    modified:   customized-ui/src/app/data/sample-data.ts

--- customized-ui/src/app/data/sample-data.ts
+++ customized-ui/src/app/data/sample-data.ts
@@ -2,11 +2,11 @@ import { Card } from 'ui-controls';
 
 export class SampleData {
   static cards: Card[] = [
-    {title:'',description:'',link:'#'},
-    {title:'',description:'',link:'#'},
-    {title:'',description:'',link:'#'},
-    {title:'',description:'',link:'#'},
-    {title:'',description:'',link:'#'},
-    {title:'',description:'',link:'#'}
+    {title:'some number one title',description:'description of some number one title',link:'#'},
+    {title:'some title number two',description:'description of some title number two',link:'#'},
+    {title:'some title number three',description:'description of some title number three',link:'#'},
+    {title:'some title number four',description:'description of some title number four',link:'#'},
+    {title:'some title number five',description:'description of some title number five',link:'#'},
+    {title:'some title number six',description:'description of some title number six',link:'#'}
   ]
 }
