bfaa97b2a541c0015d37c7c6aba6eab412fed117
paolomococci <paolomococci@gmail.com>
2022-09-30 13:27:06 +0200

    new file:   customized-ui/projects/ui-controls/src/lib/card-list/card-list.component.ts

---
+++ customized-ui/projects/ui-controls/src/lib/card-list/card-list.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'lib-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.sass']
+})
+export class CardListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
