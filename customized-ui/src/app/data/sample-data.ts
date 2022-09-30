6fc69a571d34d7a288a6293dabed7bc9d02b872d
paolomococci <paolomococci@gmail.com>
2022-09-30 21:13:26 +0200

    modified:   customized-ui/src/app/data/sample-data.ts

--- customized-ui/src/app/data/sample-data.ts
+++ customized-ui/src/app/data/sample-data.ts
@@ -9,16 +9,6 @@ export class SampleData {
     {title:'some title number five',description:'description of some title number five',link:'#'},
     {title:'some title number six',description:'description of some title number six',link:'#'},
     {title:'some title number seven',description:'description of some title number seven',link:'#'},
-    {title:'some title number eight',description:'description of some title number eight',link:'#'},
-    {title:'some title number nine',description:'description of some title number nine',link:'#'},
-    {title:'some title number ten',description:'description of some title number ten',link:'#'},
-    {title:'some title number eleven',description:'description of some title number eleven',link:'#'},
-    {title:'some title number twelve',description:'description of some title number twelve',link:'#'},
-    {title:'some title number thirteen',description:'description of some title number thirteen',link:'#'},
-    {title:'some title number fourteen',description:'description of some title number fourteen',link:'#'},
-    {title:'some title number fifteen',description:'description of some title number fifteen',link:'#'},
-    {title:'some title number sixteen',description:'description of some title number sixteen',link:'#'},
-    {title:'some title number seventeen',description:'description of some title number seventeen',link:'#'},
-    {title:'some title number eighteen',description:'description of some title number eighteen',link:'#'}
+    {title:'some title number eight',description:'description of some title number eight',link:'#'}
   ]
 }
