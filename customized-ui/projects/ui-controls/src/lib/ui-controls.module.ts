2629e7b75e0a7ab0e367efa81b6bb8f5774266b2
paolomococci <paolomococci@gmail.com>
2022-09-30 20:13:44 +0200

    modified:   customized-ui/projects/ui-controls/src/lib/ui-controls.module.ts

--- customized-ui/projects/ui-controls/src/lib/ui-controls.module.ts
+++ customized-ui/projects/ui-controls/src/lib/ui-controls.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core'
 import { UiControlsComponent } from './ui-controls.component'
 import { CardListComponent } from './card-list/card-list.component'
-
-
+import { CommonModule } from '@angular/common'
 
 @NgModule({
   declarations: [
@@ -10,6 +9,7 @@ import { CardListComponent } from './card-list/card-list.component'
     CardListComponent
   ],
   imports: [
+    CommonModule
   ],
   exports: [
     UiControlsComponent,
