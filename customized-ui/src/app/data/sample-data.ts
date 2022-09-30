8add4170f2adf08e06565cd3cc452392198dbfa1
paolomococci <paolomococci@gmail.com>
2022-09-30 21:12:41 +0200

    modified:   customized-ui/src/app/data/sample-data.ts

--- customized-ui/src/app/data/sample-data.ts
+++ customized-ui/src/app/data/sample-data.ts
@@ -7,6 +7,18 @@ export class SampleData {
     {title:'some title number three',description:'description of some title number three',link:'#'},
     {title:'some title number four',description:'description of some title number four',link:'#'},
     {title:'some title number five',description:'description of some title number five',link:'#'},
-    {title:'some title number six',description:'description of some title number six',link:'#'}
+    {title:'some title number six',description:'description of some title number six',link:'#'},
+    {title:'some title number seven',description:'description of some title number seven',link:'#'},
+    {title:'some title number eight',description:'description of some title number eight',link:'#'},
+    {title:'some title number nine',description:'description of some title number nine',link:'#'},
+    {title:'some title number ten',description:'description of some title number ten',link:'#'},
+    {title:'some title number eleven',description:'description of some title number eleven',link:'#'},
+    {title:'some title number twelve',description:'description of some title number twelve',link:'#'},
+    {title:'some title number thirteen',description:'description of some title number thirteen',link:'#'},
+    {title:'some title number fourteen',description:'description of some title number fourteen',link:'#'},
+    {title:'some title number fifteen',description:'description of some title number fifteen',link:'#'},
+    {title:'some title number sixteen',description:'description of some title number sixteen',link:'#'},
+    {title:'some title number seventeen',description:'description of some title number seventeen',link:'#'},
+    {title:'some title number eighteen',description:'description of some title number eighteen',link:'#'}
   ]
 }
