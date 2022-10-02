4705a5f43a05555cc2a5955e466140ade56fb981
paolomococci <paolomococci@gmail.com>
2022-10-02 10:36:30 +0200

    modified:   customized-ui/projects/ui-sample/src/app/app.module.ts

--- customized-ui/projects/ui-sample/src/app/app.module.ts
+++ customized-ui/projects/ui-sample/src/app/app.module.ts
@@ -3,10 +3,13 @@ import {
   NgModule
 } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { createCustomElement } from '@angular/elements'
-
-
 import { AppRoutingModule } from './app-routing.module'
+
+import { createCustomElement } from '@angular/elements'
+import {
+  UiControlsModule,
+  CopyButtonComponent
+} from 'ui-controls'
 
 @NgModule({
   declarations: [],
