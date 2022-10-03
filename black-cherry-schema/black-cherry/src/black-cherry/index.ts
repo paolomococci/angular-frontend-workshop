20bf1850abbca88af1644ed031218ceec49a063d
paolomococci <paolomococci@gmail.com>
2022-10-03 08:26:55 +0200

    modified:   black-cherry-schema/black-cherry/src/black-cherry/index.ts

--- black-cherry-schema/black-cherry/src/black-cherry/index.ts
+++ black-cherry-schema/black-cherry/src/black-cherry/index.ts
@@ -4,11 +4,11 @@ import {
   Tree
 } from '@angular-devkit/schematics'
 
+import { Schema } from './schema'
+
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function blackCherry(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree
-  }
+export function blackCherry(_options: Schema): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {}
 }
