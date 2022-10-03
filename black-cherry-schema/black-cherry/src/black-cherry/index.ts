068f20b75ebaaf515c42abd4e8c36a220ceeefb1
paolomococci <paolomococci@gmail.com>
2022-10-03 21:05:30 +0200

    modified:   black-cherry-schema/black-cherry/src/black-cherry/index.ts

--- black-cherry-schema/black-cherry/src/black-cherry/index.ts
+++ black-cherry-schema/black-cherry/src/black-cherry/index.ts
@@ -21,5 +21,18 @@ import { Schema } from './schema'
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function blackCherry(_options: Schema): Rule {
-  return (_tree: Tree, _context: SchematicContext) => {}
+  return (_tree: Tree, _context: SchematicContext) => {
+    _options.path = _options.path ?? normalize('src/app/' + _options.name as string)
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        classify: strings.classify,
+        dasherize: strings.dasherize,
+        name: _options.name
+      }),
+      move(normalize(_options.path as string))
+    ])
+    return chain([
+      mergeWith(templateSource)
+    ])
+  }
 }
