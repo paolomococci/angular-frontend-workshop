6c241ec0729e811696741b3da0c56cc6e96e3b00
paolomococci <paolomococci@gmail.com>
2022-10-03 06:57:40 +0200

    modified:   black-cherry/src/black-cherry/index_spec.ts

--- black-cherry/src/black-cherry/index_spec.ts
+++ black-cherry/src/black-cherry/index_spec.ts
@@ -1,16 +1,16 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import * as path from 'path';
+import { Tree } from '@angular-devkit/schematics'
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing'
+import * as path from 'path'
 
-const collectionPath = path.join(__dirname, '../collection.json');
+const collectionPath = path.join(__dirname, '../collection.json')
 
 describe('black-cherry', () => {
   it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const runner = new SchematicTestRunner('schematics', collectionPath)
     const tree = await runner
       .runSchematicAsync('black-cherry', {}, Tree.empty())
-      .toPromise();
+      .toPromise()
 
-    expect(tree.files).toEqual([]);
-  });
-});
+    expect(tree.files).toEqual([])
+  })
+})
