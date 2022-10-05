aac1c5139b0146ebbb7dc5eabf716ea42027202d
paolomococci <paolomococci@gmail.com>
2022-10-06 01:14:18 +0200

    modified:   narrow-app/src/app/app.module.ts

--- narrow-app/src/app/app.module.ts
+++ narrow-app/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { AppComponent } from './app.component'
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
