669d8249dfdfd61d6e5b1ed3d68495f66992f5be
paolomococci <paolomococci@gmail.com>
2022-10-06 02:06:46 +0200

    modified:   narrow-app/src/app/app.module.ts

--- narrow-app/src/app/app.module.ts
+++ narrow-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
+import { PersonalInfoComponent } from './personal-info/personal-info.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInfoComponent
   ],
   imports: [
     BrowserModule,
