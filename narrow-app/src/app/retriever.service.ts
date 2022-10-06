038e07cdf342620ca86eae53f7cb692a863a8ef0
paolomococci <paolomococci@gmail.com>
2022-10-06 10:25:49 +0200

    modified:   narrow-app/src/app/retriever.service.ts

--- narrow-app/src/app/retriever.service.ts
+++ narrow-app/src/app/retriever.service.ts
@@ -13,6 +13,7 @@ export class RetrieverService {
   constructor(private httpClient: HttpClient) { }
 
   // method to getting data of user
+  // format: `${environment.apiUrl}/users/${environment.username}`
   getUser(): Observable<User> {
     return this.httpClient.get<User>(
       `${environment.apiUrl}/users/${environment.username}`
