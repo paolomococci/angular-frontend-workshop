180e3b41df909cc718d5882c9080d930b42c3257
paolomococci <paolomococci@gmail.com>
2022-10-09 19:02:27 +0200

    modified:   narrow-app/src/app/retriever.service.ts

--- narrow-app/src/app/retriever.service.ts
+++ narrow-app/src/app/retriever.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams
+} from '@angular/common/http'
 import { Observable } from 'rxjs'
 
 import { User } from './user'
